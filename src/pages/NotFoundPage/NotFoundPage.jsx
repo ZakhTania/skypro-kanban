02d4a7d18e28bec69314b1d676ef3d1bdb4487da
ZakhTania <tatizarova@gmail.com>
2023-12-27 10:00:47 +0300

    fix img path

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,28 +1,23 @@
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../lib/appRoutes";
-import "./NotFoundPage.css"
-
+import "./NotFoundPage.css";
 
 export default function NotFound() {
-    return (  
-   
-        <div className="wrapper">
-            
-        <div className="container-notFound">
-           
-          <div className="modal">
-            <div className="modal__block">
-              <div className="modal__ttl">
-                 <img src="../../../public/images/404.png
-            " alt="error"/> 
-                <h2>Страница не найдена</h2>
-              </div>
-              <button className="modal__btn-enter _hover01" id="btnEnter">
-              <Link to={AppRoutes.HOME}>Перейти на главную</Link>
-              </button>
+  return (
+    <div className="wrapper">
+      <div className="container-notFound">
+        <div className="modal">
+          <div className="modal__block">
+            <div className="modal__ttl">
+              <img src="images/404.png" alt="error" />
+              <h2>Страница не найдена</h2>
             </div>
+            <button className="modal__btn-enter _hover01" id="btnEnter">
+              <Link to={AppRoutes.HOME}>Перейти на главную</Link>
+            </button>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
