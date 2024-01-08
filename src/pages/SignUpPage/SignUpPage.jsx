4f29b3e7bc92903846530d50bdfdf9ff28c73ec6
ZakhTania <tatizarova@gmail.com>
2024-01-08 19:13:20 +0300

    Add styled components to SignUpPage

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,54 +1,43 @@
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../lib/appRoutes";
-import "./SignUpPage.css";
+import {
+  ContainerSignUp,
+  Modal,
+  ModalBlock,
+  ModalBtnSignUpEnt,
+  ModalFormGroup,
+  ModalFormLogin,
+  ModalTtl,
+  Wrapper,
+} from "./SignUpPage.styled";
+import Input from "../../components/Common/Input/Input";
 
-export default function SignUp({setAuth}) {
+export default function SignUp({ setAuth }) {
   return (
-    <div className="wrapper">
-      <div className="container-signup">
-        <div className="modal">
-          <div className="modal__block">
-            <div className="modal__ttl">
+    <Wrapper>
+      <ContainerSignUp>
+        <Modal>
+          <ModalBlock>
+            <ModalTtl>
               <h2>Регистрация</h2>
-            </div>
-            <form className="modal__form-login" id="formLogUp" action="#">
-              <input
-                className="modal__input first-name"
-                type="text"
-                name="first-name"
-                id="first-name"
-                placeholder="Имя"
-              />
-              <input
-                className="modal__input login"
-                type="text"
-                name="login"
-                id="loginReg"
-                placeholder="Эл. почта"
-              />
-              <input
-                className="modal__input password-first"
-                type="password"
-                name="password"
-                id="passwordFirst"
-                placeholder="Пароль"
-              />
-              <button
-                className="modal__btn-signup-ent _hover01"
-                id="SignUpEnter"
-                onClick={setAuth}
-              >Зарегистрироваться
-              </button>
-              <div className="modal__form-group">
+            </ModalTtl>
+            <ModalFormLogin>
+              <Input type="text" name="first-name" placeholder="Имя" />
+              <Input type="text" name="login" placeholder="Эл. почта" />
+              <Input type="password" name="password" placeholder="Пароль" />
+              <ModalBtnSignUpEnt onClick={setAuth}>
+                Зарегистрироваться
+              </ModalBtnSignUpEnt>
+              <ModalFormGroup>
                 <p>
                   Уже есть аккаунт?{" "}
                   <Link to={AppRoutes.SIGNIN}>Войдите здесь</Link>
                 </p>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+              </ModalFormGroup>
+            </ModalFormLogin>
+          </ModalBlock>
+        </Modal>
+      </ContainerSignUp>
+    </Wrapper>
   );
 }
