1d79e25b78db8eae792fe8f528d1800541096e1d
ZakhTania <tatizarova@gmail.com>
2024-01-08 19:29:47 +0300

    Styled components to SignIn Page

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,44 +1,40 @@
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../lib/appRoutes";
-import "./SignInPage.css"
+import {
+  ContainerSignUp,
+  Modal,
+  ModalBlock,
+  ModalBtnSignUpEnt,
+  ModalFormGroup,
+  ModalFormLogin,
+  ModalTtl,
+  Wrapper,
+} from "./SignInPage.styled";
+import Input from "../../components/Common/Input/Input";
 
-export default function SignIn({setAuth}) {
+export default function SignIn({ setAuth }) {
   return (
-    <div className="wrapper">
-      <div className="container-signin">
-        <div className="modal">
-          <div className="modal__block">
-            <div className="modal__ttl">
+    <Wrapper>
+      <ContainerSignUp>
+        <Modal>
+          <ModalBlock>
+            <ModalTtl>
               <h2>Вход</h2>
-            </div>
-            <form className="modal__form-login" id="formLogIn" action="#">
-              <input
-                className="modal__input"
-                type="text"
-                name="login"
-                id="formlogin"
-                placeholder="Эл. почта"
-              />
-              <input
-                className="modal__input"
-                type="password"
-                name="password"
-                id="formpassword"
-                placeholder="Пароль"
-              />
-              <button className="modal__btn-enter _hover01" id="btnEnter" onClick={setAuth}>
-              {/* <button className="modal__btn-enter _hover01" id="btnEnter"> */}
-                {/* <Link to={AppRoutes.HOME}>Войти</Link> */}
-                {/* <a href="../main.html"></a> */}Войти
-              </button>
-              <div className="modal__form-group">
-                <p>Нужно зарегистрироваться?</p>
-                <Link to={AppRoutes.SIGNUP}>Регистрируйтесь здесь</Link>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+            </ModalTtl>
+            <ModalFormLogin>
+              <Input type="text" name="login" placeholder="Эл. почта" />
+              <Input type="password" name="password" placeholder="Пароль" />
+              <ModalBtnSignUpEnt onClick={setAuth}>Войти</ModalBtnSignUpEnt>
+              <ModalFormGroup>
+                <p>
+                  Нужно зарегистрироваться?
+                  <Link to={AppRoutes.SIGNUP}>Регистрируйтесь здесь</Link>
+                </p>
+              </ModalFormGroup>
+            </ModalFormLogin>
+          </ModalBlock>
+        </Modal>
+      </ContainerSignUp>
+    </Wrapper>
   );
 }
---
+++ src/pages/SignInPage/SignInPage.styled.js
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import { hover01 } from "../../components/Common/Common.styled";
+import { breakpoints } from "../../lib/breakpoints";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  background-color: #eaeef6;
+`;
+
+export const ContainerSignUp = styled.div`
+  display: block;
+  width: 100vw;
+  min-height: 100vh;
+  margin: 0 auto;
+`;
+
+export const Modal = styled.div`
+  width: 100%;
+  height: 100%;
+  min-width: 320px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: ${breakpoints.sm}px) {
+    background-color: #ffffff;
+  }
+`;
+
+export const ModalBlock = styled.div`
+  display: block;
+  margin: 0 auto;
+  background-color: #ffffff;
+  max-width: 368px;
+  width: 100%;
+  padding: 50px 60px;
+  border-radius: 10px;
+  border: 0.7px solid #d4dbe5;
+  box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13);
+
+  @media screen and (max-width: ${breakpoints.sm}px) {
+    max-width: 368px;
+    width: 100%;
+    padding: 0 16px;
+    border-radius: none;
+    border: none;
+    box-shadow: none;
+  }
+`;
+
+export const ModalTtl = styled.div`
+  & h2 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 30px;
+    letter-spacing: -0.6px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const ModalBtnSignUpEnt = styled.button`
+  width: 100%;
+  height: 30px;
+  background-color: #565eef;
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #ffffff;
+
+  ${hover01}
+
+  @media screen and (max-width: ${breakpoints.sm}px) {
+    height: 40px;
+  }
+`;
+
+export const ModalFormLogin = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & input:not(:last-child) {
+    margin-bottom: 7px;
+  }
+`;
+
+export const ModalFormGroup = styled.div`
+  text-align: center;
+
+  & p,
+  a {
+    color: rgba(148, 166, 190, 0.4);
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 150%;
+    letter-spacing: -0.14px;
+  }
+  & a {
+    text-decoration: underline;
+  }
+`;
