3dbd4adb9072eefc59304d451d1c536ffb753fcb
ZakhTania <tatizarova@gmail.com>
2024-01-29 13:33:07 +0300

    add err's prop to Input

--- src/components/Common/Input/Input.jsx
+++ src/components/Common/Input/Input.jsx
@@ -1,5 +1,5 @@
 import { StyledInput } from "./Input.styled";
 
-export default function Input({ type, name, placeholder, value, onChange }) {
-  return <StyledInput type={type} name={name} placeholder={placeholder} value={value} onChange={onChange}/>;
+export default function Input({ type, name, placeholder, value, onChange, onClick, err }) {
+  return <StyledInput type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} $err={err} onClick={onClick} required/>;
 }
