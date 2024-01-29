f257bfd2a0ebcdf81c7874438ae7f048246c857e
ZakhTania <tatizarova@gmail.com>
2024-01-29 13:33:40 +0300

    fix dropdown

--- src/components/PopUserSet/PopUserSet.jsx
+++ src/components/PopUserSet/PopUserSet.jsx
@@ -10,18 +10,24 @@ import {
   PopUserSetThemeP,
 } from "./PopUserSet.styled";
 import useUser from "../../hooks/useUser";
+import useTheme from "../../hooks/useTheme";
 
-function PopUserSet() {
 
-const { user } = useUser();
+function PopUserSet({ toggleDropdown }) {
+  const { user } = useUser();
+const { toggleTheme } = useTheme();
 
+function setTheme() {
+  toggleTheme();
+  toggleDropdown();
+}
   return (
     <HeaderPopUserSet>
       <PopUserSetName>{user.name}</PopUserSetName>
       <PopUserSetMail>{user.login}</PopUserSetMail>
       <PopUserSetTheme>
         <PopUserSetThemeP>Темная тема</PopUserSetThemeP>
-        <PopUserSetThemeInput type="checkbox" />
+        <PopUserSetThemeInput type="checkbox" onClick={setTheme}/>
       </PopUserSetTheme>
       <Link to={AppRoutes.EXIT}>
         <PopUserSetBtn>Выйти</PopUserSetBtn>
