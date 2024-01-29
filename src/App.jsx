71c2e2d2312bdc904430e2da8df9e10c0eb99a10
ZakhTania <tatizarova@gmail.com>
2024-01-29 13:20:39 +0300

    del unnecessary .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { AppRoutes } from "./lib/appRoutes";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
