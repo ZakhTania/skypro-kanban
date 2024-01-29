fcfe5e6d3ce8e511966ba2bc1ca0dfb7c3f08f1f
ZakhTania <tatizarova@gmail.com>
2024-01-29 13:26:33 +0300

    switch img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,22 +11,37 @@ import { Container } from "../Common/Common.styled";
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../lib/appRoutes";
 import useUser from "../../hooks/useUser";
+import {  useState } from "react";
+import { useEffect } from "react";
+import useTheme from "../../hooks/useTheme";
 
-function Header({ isOpen, toggleDropdown}) {
+
+function Header({ isOpen, toggleDropdown }) {
+  const { user } = useUser();
+  const { theme } = useTheme();
+  const [ imgURL, setImgURL ] = useState("images/logo.png");
+
+  useEffect(() => {
+  if (theme === "light") {
+      setImgURL("images/logo.png");
+    } else {
+      setImgURL("images/logo_dark.png");
+    }
+  }, [theme])
   
-  const {user} = useUser();
 
   return (
     <StyledHeader>
       <Container>
         <HeaderBlock>
           <HeaderLogo>
-            <a href="" target="_self">
-              <img src="images/logo.png" alt="logo" />
-            </a>
+            <Link to={AppRoutes.HOME}>
+              <img src={imgURL} alt="logo" />
+            </Link>
           </HeaderLogo>
           {/* <div className="header__logo _dark">
             <a href="" target="_self">
+                     <img src="images/logo.png" alt="logo" />   
               <img src="images/logo_dark.png" alt="logo" />
             </a>
           </div> */}
@@ -35,7 +50,7 @@ function Header({ isOpen, toggleDropdown}) {
               <HeaderBtnNew> Создать новую задачу</HeaderBtnNew>
             </Link>
             <HeaderUser onClick={toggleDropdown}>{user.name}</HeaderUser>
-            {isOpen && <PopUserSet />}
+            {isOpen && <PopUserSet toggleDropdown={toggleDropdown} />}
           </HeaderNav>
         </HeaderBlock>
       </Container>
